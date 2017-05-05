968dc792792d48ac571f6d60ce0adfa755782f6f
Evgenij Merchansky <evgenij.merchansky@gmail.com>
2017-05-05 23:49:47 +0300

    no added correc axios request, so close

--- src/app/actions-generators/someAction.js
+++ src/app/actions-generators/someAction.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
 
 export const query = (arg) => {
+    console.log('arg -> ', arg)
   return function(dispatch){
     const q = arg;
-    const generalRequest = axios.get('https://www.googleapis.com/youtube/v3')
-    // /-/-/-/-/-/-/-/- построить запрос по поиску с переменной "q" ! ! !
-    // const int = gapi.client.youtube.search.list({
-    //   q: q,
-    //   part: 'snippet'
-    // });
-    // /-/-/-/-/-/-/-/- построить запрос по поиску с переменной "q" ! ! !
+    const generalRequest = axios.get('https://www.googleapis.com/youtube/v3/search',
+      {
+        data: {
+          part : 'id ,snippet',
+          q: q,
+          type: 'video',
+          key: 'AIzaSyDidihTEViX7bkm17xLglRF51mjonDSw-I'
+        }
+      }
+    )
+    console.log(generalRequest);
     dispatch({
       type: 'ADD_VALUE',
       payload: generalRequest
